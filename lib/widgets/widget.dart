40f33e2048d3ff2b6f218716a938a057c077cf34
berkaygurcesme <berkaygurcesme@hotmail.com>
2022-09-12 23:45:10 +0300

    first widger

---
+++ lib/widgets/widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class firstWidget extends StatefulWidget {
+  firstWidget({Key? key}) : super(key: key);
+
+  @override
+  State<firstWidget> createState() => _firstWidgetState();
+}
+
+class _firstWidgetState extends State<firstWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
